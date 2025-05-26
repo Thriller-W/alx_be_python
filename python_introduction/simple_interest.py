5fdd4ecc18cbe34d99fc0909a5e21690add68806
Thriller-W <githinjimurage24@gmail.com>
2025-05-26 19:10:05 +0300

    Add simple_interest.py for interest calculation task

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+## Define Variables
+
+principal = 1000
+rate = 0.05
+time = 3
+
+## Calculations for Interest
+
+interest = principal * rate* time
+
+##Results
+
+print(f"The simple interest is: {interest}")
