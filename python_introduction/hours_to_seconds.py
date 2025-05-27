9d31beefe934c0381a8690ab4a5e92cca8550a91
Thriller-W <githinjimurage24@gmail.com>
2025-05-27 12:14:03 +0300

    Add hours_to_seconds.py conversion task

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,11 @@
+## Number of hours
+hours = 2
+
+#Number of seconds in one hour
+seconds_per_hour = 3600
+
+##Calculations for total number of seconds
+seconds = hours * seconds_per_hour
+
+##Results
+print(f"{hours} hour(s) is {seconds} seconds.")
