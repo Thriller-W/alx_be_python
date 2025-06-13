fc8ec0f4d983618755230dd95d05fd2bc1f74e94
Thriller-W <githinjimurage24@gmail.com>
2025-06-13 14:49:50 +0300

    Explore datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+## Display current date and time
+
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    return formatted_date
+
+## Calculate future date
+def calculate_future_date(days_to_add):
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days_to_add)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    return formatted_future_date
+
+
